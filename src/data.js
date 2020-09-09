2c2d24510879b6ba045c142c7b527ba48ad62afe
Manish <manish.kumar@aline-consulting.com>
2020-09-10 03:01:34 +0530

    sample list

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+        blockName: 'Block One',
+        details: [
+            { segments: [{width: 30, color: '#DD8A54'}, {width: 20, color: '#DDB94F'}, {width: 50, color: '#3E894E'}], content: 'Content 1'},
+            { segments: [{width: 10, color: '#DD8A54'}, {width: 10, color: '#DDB94F'}, {width: 80, color: '#3E894E'}], content: 'Content 2'},
+            { segments: [{width: 20, color: '#DD8A54'}, {width: 30, color: '#DDB94F'}, {width: 50, color: '#3E894E'}], content: 'Content 3'},
+        ]
+    },
+    {
+        blockName: 'Block Two',
+        details: [
+            { segments: [{width: 5, color: '#DD8A54'}, {width: 45, color: '#DDB94F'}, {width: 50, color: '#3E894E'}], content: 'Content 4'},
+            { segments: [{width: 60, color: '#DD8A54'}, {width: 25, color: '#DDB94F'}, {width: 15, color: '#3E894E'}], content: 'Content 5'},
+            { segments: [{width: 30, color: '#DD8A54'}, {width: 30, color: '#DDB94F'}, {width: 40, color: '#3E894E'}], content: 'Content 6'},
+        ]
+    },
+    {
+        blockName: 'Block Three',
+        details: [
+            { segments: [{width: 25, color: '#DD8A54'}, {width: 25, color: '#DDB94F'}, {width: 50, color: '#3E894E'}], content: 'Content 7'},
+            { segments: [{width: 30, color: '#DD8A54'}, {width: 35, color: '#DDB94F'}, {width: 35, color: '#3E894E'}], content: 'Content 8'},
+            { segments: [{width: 20, color: '#DD8A54'}, {width: 10, color: '#DDB94F'}, {width: 70, color: '#3E894E'}], content: 'Content 9'},
+        ]
+    },
+    {
+        blockName: 'Block Four',
+        details: [
+            { segments: [{width: 30, color: '#DD8A54'}, {width: 20, color: '#DDB94F'}, {width: 50, color: '#3E894E'}], content: 'Content 10'},
+            { segments: [{width: 5, color: '#DD8A54'}, {width: 90, color: '#DDB94F'}, {width: 5, color: '#3E894E'}], content: 'Content 11'},
+            { segments: [{width: 15, color: '#DD8A54'}, {width: 45, color: '#DDB94F'}, {width: 40, color: '#3E894E'}], content: 'Content 12'},
+        ]
+    }
+];
+
+export default data;
